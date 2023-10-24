142ac328b8dae328297b5c81ab4961e4b8fc02be
agusspuertas <agustinnpuertas@gmail.com>
2023-10-23 23:56:37 -0300

    Add map in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div id='footer' className='bg-gray-400'>Footer</div>
+    <section id='footer' className='bg-[#F5F5F5] h-72 flex  justify-center' >
+      
+      <article className=''>
+      <h1 className='text-2xl font-serif'>Mostacho Barberia</h1>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3349.2928159974267!2d-68.85213642514675!3d-32.916861070383405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e097c5b4da353%3A0xc286f99f89eddd3f!2sMostacho%20Barberia!5e0!3m2!1ses!2sar!4v1698115979310!5m2!1ses!2sar" width="225" height="225"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </article>
+    </section>
   )
 }
 
