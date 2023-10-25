08ddd30a3fccf3ea6018d46eee1181b74a2b48d2
agusspuertas <agustinnpuertas@gmail.com>
2023-10-24 22:52:32 -0300

    Update in the position of the "pedir turno" button

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -4,7 +4,7 @@ import Formulario from './Form'
 function Home() {
   return (
  <main className='min-h-screen flex justify-center bg-gradient-to-b from-[#EDDED4] to-[#F3E9E2] '> 
-  <div className='mt-10'>
+  <div className='mt-auto mb-24'>
     <div className='w-[12vw] bg-[#F5F5F5] rounded-2xl'>
     </div>
        <Formulario/>
