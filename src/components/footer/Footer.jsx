c3a3847820024f5d6479b307509bafe7ae8f6880
agusspuertas <agustinnpuertas@gmail.com>
2023-11-01 17:14:30 -0300

    Fix the width of the map in the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,16 +4,16 @@ function Footer() {
   return (
     <section id='footer' className='bg-gradient-to-t from-[#E1C9B7] to-[#F3E9E2] min-h-full flex flex-col min-w-full' >
 
-      <article className='h-full flex items-center justify-around'>
+      <article className='  flex flex-col justify-around h-full'>
 
-        <div className=' border border-gray-600'>
-          <h1 className='text-2xl bg-[#383D42] text-[#F5F5F5] text-center font-serif'>Mostacho Barberia</h1>
+        <div className='w-fit border border-gray-600'>
+          <h1 className='text-2xl bg-[#383D42] w-full text-[#F5F5F5] text-center font-serif'>Mostacho Barberia</h1>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3349.2928159974267!2d-68.85213642514675!3d-32.916861070383405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e097c5b4da353%3A0xc286f99f89eddd3f!2sMostacho%20Barberia!5e0!3m2!1ses!2sar!4v1698115979310!5m2!1ses!2sar" width="300" height="200" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
         <div className='w-[20rem] h-[11rem] ml-7 rounded-xl shadow-lg shadow-gray-400 bg-[#383D42] my-auto'>
           <h1 className='text-2xl bg-[#383D42] text-[#F5F5F5] text-center rounded-t-xl font-serif'>Ubicación📍</h1>
-          <ul className='text-[#e8d1c1] font-sans font-light  mt-3'>
+          <ul className='text-[#F5F5F5] font-sans font-light  mt-3'>
             <li className='ml-5'>-Chacabuco 232</li>
             <li className='ml-5'>-Godoy Cruz</li>
             <li className='ml-5'>-A una cuadra de la ciclovía</li>
